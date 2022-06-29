d96dc4d8a98af04b6f4cdda1b57f11d4c1fa4eaf
Lucas Sweeney <lsweeney0124@gmail.com>
2022-06-29 18:32:59 -0500

    commit tree 95a7a5c9385cb12e15924c59c131d7f5643da89f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+class CashRegister
+
+  attr_accessor :total, :discount, :title, :price, :quantity, :items
+
+  def initialize discount = 0
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item title, price, quantity = 1
+    
+    @title = title
+    @price = price
+    @quantity = quantity
+    @total += (price * quantity)
+    quantity.times do 
+      @items << title
+    end
+    
+  end
+
+  def apply_discount 
+    if discount > 0
+      @total = @price - (@price * (@discount.to_f / 100))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total -= (price * quantity)
+  end
+
+end
 
